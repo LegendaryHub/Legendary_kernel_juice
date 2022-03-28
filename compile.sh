498ce4133d406894e4ac0f0af36cc5e7032a40e2
RE800T <brutalman122@gmail.com>
2022-03-28 10:29:59 +0000

    Updating build script
    Cloning AnyKernel
    Clearing AnyKernel when rebuilding the kernel
    New zip file name
    
    Signed-off-by: RE800T <brutalman122@gmail.com>

--- compile.sh
+++ compile.sh
@@ -1,81 +1,67 @@
-#!/bin/bash
-
-# Цвета
-green='\033[01;32m'
-red='\033[01;31m'
-blink_red='\033[05;31m'
-restore='\033[0m'
-# Создание папки
-mkdir -p ~/toolchain
-
-echo -e "${green}"
-echo "~~~~~~~~~~~~~~~"
-echo "Creating folder:"
-echo "~~~~~~~~~~~~~~~"
-echo -e "${restore}"
-
-echo -e "${green}"
-echo "~~~~~~~~~~~~~~~~~~~~~"
-echo "Cloning dependencies:"
-echo "~~~~~~~~~~~~~~~~~~~~~"
-echo -e "${restore}"
-
-# Клонирование proton-clang
-
-git clone --depth=1 https://github.com/silont-project/silont-clang ~/toolchain/clang
-
-# Клонирование GCC 64bit
+echo "Cloning dependencies"
+git clone --depth=1 https://github.com/kdrag0n/proton-clang ~/toolchain/clang
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 ~/toolchain/gcc
-
-# Клонирование GCC 32bit
 git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 ~/toolchain/gcc32
-
-echo -e "${green}"
-echo "~~~~"
-echo "Done:"
-echo "~~~~"
-echo -e "${restore}"
+git clone --depth=1 https://github.com/LegendaryHub/AnyKernel3 ~/toolchain/AnyKernel3
+echo "Done"
 
 KERNEL_DIR=$(pwd)
 IMAGE="${KERNEL_DIR}/out/arch/arm64/boot/Image"
 TANGGAL=$(date +"%Y%m%d-%H")
 BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 PATH="${HOME}/toolchain/clang/bin:${HOME}/toolchain/gcc/bin:${HOME}/toolchain/gcc32/bin:${PATH}"
-export KBUILD_COMPILER_STRING="$(${HOME}/toolchain/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
-export KBUILD_BUILD_USER=Evgeny
-export KBUILD_BUILD_HOST=Sezam4ik
+export ARCH=arm64
+export KBUILD_BUILD_USER=Sezam4ik
+export KBUILD_BUILD_HOST=Instance
 
 make O=out ARCH=arm64 vendor/lime-perf_defconfig
 
-# Время начала сборки ядра
-DATE_START=$(date +"%s")
-
 # Compile plox
 compile() {
-	   make -j$(nproc) O=out \
+    make -j$(nproc) O=out \
                     ARCH=arm64 \
                     CC=clang \
-                    OBJCOPY=llvm-objcopy \
-                    OBJDUMP=llvm-objdump \
-		    CROSS_COMPILE=aarch64-linux-android- \
-                    CROSS_COMPILE_ARM32=arm-linux-androideabi- \
-                    CLANG_TRIPLE=aarch64-linux-gnu-
-		    LD="ccache ld.lld" \
-		    AR=llvm-ar \
-                    NM=llvm-nm \
-                    OBJCOPY=llvm-objcopy \
-                    OBJDUMP=llvm-objdump \
-                    STRIP=llvm-strip $1 $2 $3
+		    ARCH=arm64 \
+		    CC="ccache clang" \
+		    CXX="ccache. clang++" \
+		    AR="ccache llvm-ar" \
+		    AS="ccache llvm-as" \
+		    NM="ccache llvm-nm" \
+		    STRIP="ccache llvm-strip" \
+		    OBJCOPY="ccache llvm-objcopy" \
+		    OBJDUMP="ccache llvm-objdump"\
+		    OBJSIZE="ccache llvm-size" \
+		    READELF="ccache llvm-readelf" \
+		    HOSTCC="ccache clang" \
+		    HOSTCXX="ccache clang++" \
+		    HOSTAR="ccache llvm-ar" \
+		    HOSTAS="ccache llvm-as" \
+		    HOSTNM="ccache llvm-nm" \
+		    HOSTLD="ccache ld.lld" \
+		    CROSS_COMPILE=aarch64-linux-gnu- \
+		    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+		    $@
 }
 
-compile 
-# Время окончания сборки ядра
-DATE_END=$(date +"%s")
-DIFF=$(($DATE_END - $DATE_START))
 
-echo -e "${green}"
-echo "----------------------------------------------"
-echo "Build Completed in: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
-echo "----------------------------------------------"
-echo -e "${restore}"
+module() {
+[ -d "modules" ] && rm -rf modules || mkdir -p modules
 
+compile \
+INSTALL_MOD_PATH=../modules \
+INSTALL_MOD_STRIP=1 \
+modules_install
+}
+
+# Zipping
+zipping() {
+    cd ${HOME}/toolchain/AnyKernel3 || exit 1
+    cp ${KERNEL_DIR}/out/arch/arm64/boot/Image .
+    rm -rf *.zip
+    zip -r9 OEM-LegendaryKernel-RC3-juice-$(date '+%Y%m%d-%H%M').zip *
+    cd ..
+}
+
+compile
+module
+zipping
